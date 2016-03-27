59c3db880298920f0fc4256b6c1355d2ff563130
Chen Asraf <chen@asraf.me>
2016-03-28 00:58:00 +0300

    Started working on Models

--- media_scanner/media_scanner_runner.py
+++ media_scanner/media_scanner_runner.py
@@ -1,5 +1,17 @@
 from file_observer import FileObserver
+import dispatcher
 
+class MediaScanner(threading.Thread):
+    def __init__(self):
 
-def run_all_workers():
-    FileObserver()
+    def __enter__(self):
+        self.dispatcher = dispatcher.Dispatcher()
+        self.context = zmq.Context()
+        self.subscriber = self.context.socket(zmq.SUB)
+        self.subscriber.connect("tcp://localhost:{port}".format(port=dispatcher.DEFAULT_PORT))
+        self.dispatcher.start()
+
+    def __exit__(self):
+        self.subscriber.close()
+        self.context.term()
+        self.dispatcher.close()
