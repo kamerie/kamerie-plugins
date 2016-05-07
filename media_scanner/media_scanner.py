6e35914239d3ea67d380a7916cb92e0bbfcb2f97
Dor Munis <dormunis@gmail.com>
2016-05-07 14:11:25 +0300

    Initial plugin support

--- media_scanner/media_scanner.py
+++ media_scanner/media_scanner.py
@@ -1,24 +1,24 @@
 import os
 
-from template import TemplatePlugin
-
 TYPE_MOVIE = 'movie'
 TYPE_SERIES = 'series'
 
 
-class MediaScanner(TemplatePlugin):
-    def scan_directory(self, dir, media_type):
+class MediaScanner():
+    def scan_directory(self, directory, media_type):
         if media_type not in [TYPE_MOVIE, TYPE_SERIES]:
-            self._logger.error('media_type wrong: %s' % media_type)
+            print 'media_type wrong: %s' % media_type
+            # self._logger.error('media_type wrong: %s' % media_type)
 
-        for dirname, dirnames, filenames in os.walk('.'):
-            for file in filenames:
-                attributes = self.parse_file(dirname=dirname, file=file, media_type=media_type)
-                self.add_file_to_library(file, **attributes)
+        for dirname, dirnames, filenames in os.walk(directory):
+            for filename in filenames:
+                attributes = self.parse_file(dirname=dirname, filename=filename, media_type=media_type)
+                self.add_file_to_library(filename, **attributes)
 
-    def parse_file(self, dirname, file, media_type):
-        self._logger.info('parsing %s: %s' % media_type, os.path.join(dirname, file))
+    def parse_file(self, dirname, filename, media_type):
+        print 'parsing %s: %s' % (media_type, os.path.join(dirname, filename))
+        # self._logger.info('parsing %s: %s' % media_type, os.path.join(dirname, file))
         return {}
 
-    def add_file_to_library(self, file, **attributes):
+    def add_file_to_library(self, filename, **attributes):
         pass
