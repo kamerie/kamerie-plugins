398ca5cc98372910539f98738f8d78c736ca1f29
Chen Asraf <chen@asraf.me>
2016-05-08 00:12:06 +0300

    Start working on meta fetcher

---
+++ metadata_fetcher/meta_fetcher.py
@@ -0,0 +1,6 @@
+class MetaFetcher(object):
+    def __init__(self, media_type):
+        self.media_type = media_type
+
+    def fetch_metadata(self, media_object):
+        pass
--- metadata_fetcher/plugin.py
+++ metadata_fetcher/plugin.py
@@ -1,9 +1,11 @@
 from template_plugin.template import TemplatePlugin
+from kamerie.dispatcher.media_scanner import TYPE_MOVIE, TYPE_SERIES
 
 
 class Plugin(TemplatePlugin):
     def on_message(self, message):
-        print 'akabulbul', message
+        print "Metadata Fetcher received", str(message)
+        MetaFetcher(message['type']).fetch_metadata(message)
 
 
 if __name__ == '__main__':
