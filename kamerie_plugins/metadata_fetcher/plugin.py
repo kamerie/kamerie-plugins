6ff21083b5581ddf35d592dd18df07577016bbb9
Dor Munis <dormunis@gmail.com>
2016-05-09 21:44:28 +0300

    imported TemplatePlugin from template instead of template.template

--- kamerie_plugins/metadata_fetcher/plugin.py
+++ kamerie_plugins/metadata_fetcher/plugin.py
@@ -1,10 +1,10 @@
-from kamerie.template.template import TemplatePlugin
-from kamerie_plugins.metadata_fetcher.meta_fetcher import MetaFetcher
+from kamerie.template import TemplatePlugin
+from meta_fetcher import MetaFetcher
 
 
 class Plugin(TemplatePlugin):
-    def on_message(self, message):
-        print "Metadata Fetcher received", str(message)
+    def on_message(self, message, received="Metadata Fetcher received"):
+        print received, str(message)
         MetaFetcher(message['type']).fetch_metadata(message)
 
 
